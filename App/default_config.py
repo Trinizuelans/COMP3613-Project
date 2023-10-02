2e30c05dda30c6eb96df2e7b8d1c8d069739b46e
Jus-Jar <jaredheeralal04@gmail.com>
2023-10-02 17:16:37 -0400

    connected postgres db

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://trinizuelans_postgresql_user:TJzmVePABQh2vLg4TjBQZCylUo7BzEPa@dpg-ckadi3ugtj9c73fdqbi0-a/trinizuelans_postgresql"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
-ENV = "DEVELOPMENT"
+ENV = "PRODUCTION"
